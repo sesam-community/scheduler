e802dbec6c03d895b23208f61c731fcfa0f75883
Tom Bech <tom.bech@bouvet.no>
2017-08-31 09:54:18 +0200

    Recompute graph for each start

--- service/scheduler.py
+++ service/scheduler.py
@@ -24,6 +24,9 @@ from pprint import pprint
 
 scheduler = None
 headers = None
+node_url = None
+jwt_token = None
+
 
 app = Flask(__name__)
 
@@ -131,7 +134,6 @@ class SchedulerThread:
 
 @app.route('/start', methods=['POST'])
 def start():
-
     reset_pipes = request.args.get('reset_pipes')
     delete_datasets = request.args.get('delete_datasets')
     skip_input_pipes = request.args.get('skip_input_pipes')
@@ -141,16 +143,76 @@ def start():
 
     logger.setLevel(log_level)
 
-    if reset_pipes is None and delete_datasets is not None:
-        logger.warning("delete_datasets flag ignored because reset_pipes parameter not set")
-
     global scheduler
+    global node_url
+    global jwt_token
 
-    if scheduler is None:
-        return Response(status=403, response="Bootstrap scheduler is not ready yet")
+    if scheduler is None or scheduler.status not in ["running"]:
+        if scheduler is not None:
+            scheduler.stop()
 
-    if scheduler.status not in ["running"]:
-        scheduler.stop()
+        if reset_pipes is None and delete_datasets is not None:
+            logger.warning("delete_datasets flag ignored because reset_pipes parameter not set")
+
+        api_connection = sesamclient.Connection(sesamapi_base_url=node_url, jwt_auth_token=jwt_token, timeout=60*10)
+
+        graph = Graph(api_connection)
+
+        # Compute optimal rank
+        graph.unvisitNodes()
+
+        # Gather all connected graphs
+        connected_components = 0
+        for node in graph.source_endpoints[:]:
+            if not node.visited:
+                node.floodFill(connected_components)
+                connected_components += 1
+
+        logger.info("Found %s connected components in the graph!" % connected_components)
+
+        # iterate over the connected components in the graph (the flows)
+
+        graph.unvisitNodes()
+
+        all_runner = Runner(api_connection=api_connection, pipes=[p.id for p in graph.pipes])
+        all_runner.stop_and_disable_pipes(all_runner.pipes.values())
+
+        sub_graph_runners = []
+
+        # Compute optimal ranking for all subgraphs using Tarjan's algorithm
+
+        for subgraph in range(0, connected_components):
+            logger.debug("Processing subgraph #%s..." % subgraph)
+
+            g = {}
+            for node in [node for node in graph.nodes.values() if node.tag == subgraph]:
+                edges = [n.node_id for n in set(node.sinks + node.dependees)]
+                g[node.node_id] = edges
+
+            dg = tarjan(g)
+            dg.reverse()
+
+            i = 0
+            for s in dg:
+                s.reverse()
+                dg[i] = [n[5:] for n in s if n.startswith("pipe_")]
+                i += 1
+
+            pipes = []
+            for s in dg:
+                if len(s) == 1:
+                    pipes.append(s[0])
+                    continue
+                elif len(s) > 1:
+                    pipes.extend(s)
+
+            logger.debug("Optimal sequence for subgraph #%s:\n%s" % (subgraph, pformat(pipes)))
+
+            runner = Runner(api_connection=api_connection, pipes=pipes, skip_output_pipes=True)
+            runner.subgraph = subgraph
+            sub_graph_runners.append(runner)
+
+        scheduler = SchedulerThread(sub_graph_runners)
         scheduler.start(reset_pipes=reset_pipes, delete_datasets=delete_datasets, skip_input_pipes=skip_input_pipes)
         return Response(status=200, response="Bootstrap scheduler started")
     else:
@@ -246,7 +308,6 @@ if __name__ == '__main__':
         rotating_logfile_handler.setFormatter(logging.Formatter(format_string))
         logger.addHandler(rotating_logfile_handler)
 
-
     logger.setLevel(log_level)
 
     node_url = os.environ.get("URL")
@@ -262,66 +323,5 @@ if __name__ == '__main__':
         sys.exit(1)
 
     logger.info("Master API endpoint is: %s" % node_url)
-
-    api_connection = sesamclient.Connection(sesamapi_base_url=node_url, jwt_auth_token=jwt_token, timeout=60*10)
-
-    graph = Graph(api_connection)
-
-    # Compute optimal rank
-    graph.unvisitNodes()
-
-    # Gather all connected graphs
-    connected_components = 0
-    for node in graph.source_endpoints[:]:
-        if not node.visited:
-            node.floodFill(connected_components)
-            connected_components += 1
-
-    logger.info("Found %s connected components in the graph!" % connected_components)
-
-    # iterate over the connected components in the graph (the flows)
-
-    graph.unvisitNodes()
-
-    all_runner = Runner(api_connection=api_connection, pipes=[p.id for p in graph.pipes])
-    all_runner.stop_and_disable_pipes(all_runner.pipes.values())
-
-    sub_graph_runners = []
-
-    # Compute optimal ranking for all subgraphs using Tarjan's algorithm
-
-    for subgraph in range(0, connected_components):
-        logger.debug("Processing subgraph #%s..." % subgraph)
-
-        g = {}
-        for node in [node for node in graph.nodes.values() if node.tag == subgraph]:
-            edges = [n.node_id for n in set(node.sinks + node.dependees)]
-            g[node.node_id] = edges
-
-        dg = tarjan(g)
-        dg.reverse()
-
-        i = 0
-        for s in dg:
-            s.reverse()
-            dg[i] = [n[5:] for n in s if n.startswith("pipe_")]
-            i += 1
-
-        pipes = []
-        for s in dg:
-            if len(s) == 1:
-                pipes.append(s[0])
-                continue
-            elif len(s) > 1:
-                pipes.extend(s)
-
-        logger.debug("Optimal sequence for subgraph #%s:\n%s" % (subgraph, pformat(pipes)))
-
-        runner = Runner(api_connection=api_connection, pipes=pipes, skip_output_pipes=True)
-        runner.subgraph = subgraph
-        sub_graph_runners.append(runner)
-
-    scheduler = SchedulerThread(sub_graph_runners)
-    #scheduler.start()
 
     app.run(debug=False, host='0.0.0.0', port=5000)
